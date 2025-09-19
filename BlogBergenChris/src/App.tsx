72d0fa1e7d412157603564e9d72e0a7598b759a0
Bergen Chris <bergen.chris@live.be>
2025-09-19 13:17:14 +0100

    hamburgerMenuOpenOnStart

--- BlogBergenChris/src/App.tsx
+++ BlogBergenChris/src/App.tsx
@@ -7,7 +7,7 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [menuOpen, setMenuOpen] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(true);
 
   return (
     <BrowserRouter>
